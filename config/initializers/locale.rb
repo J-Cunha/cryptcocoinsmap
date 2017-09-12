45bb23008fa0b0216a35a62f6b9d3de4c71a8785
J-Cunha <soares.cunha.joao@gmail.com>
2017-09-12 09:05:54 -0300

    Updated locale selector, added new locales" "

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,10 @@
-I18n.available_locales = [:en, :pt_BR, :es]
+I18n.available_locales = [:en, #english
+                          :pt_BR, #portuguese brazil
+                          :es, #spanish
+                          :it, #italian
+                          :ge, #german
+                          :fr, #french
+                          :ja, #japanese
+                          :hi, #hindi
+                          :ar  #arabic
+]
