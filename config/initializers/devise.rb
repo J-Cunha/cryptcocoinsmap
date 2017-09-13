eb825dbae090c342cfd4c282cde7a58bd7367722
Neil R. Zamora <neil.r.zamora@gmail.com>
2017-09-13 17:03:55 +0800

    authentication with github

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,6 +253,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'users,public_repo'
   config.omniauth :facebook,      ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'],   ENV['GOOGLE_APP_SECRET']
+  config.omniauth :github,        ENV['GITHUB_APP_ID'],   ENV['GITHUB_APP_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
