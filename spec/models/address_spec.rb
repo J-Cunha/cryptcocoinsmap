de7afff0df49eb24a0738d281b2c22d18c9330bc
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-20 21:34:25 -0300

    Factories and testing working

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -14,14 +14,4 @@ RSpec.describe Address, :type => :model do
     it { should have_many(:address_categories) }
     it { should have_many(:categories) }
   end
-
-  context "validating active_record validations" do
-    it { expect(subject).to validate_presence_of(:business_name) }
-    it { should validate_presence_of(:country) }
-    it { is_expected.to validate_presence_of(:state) }
-    it { is_expected.to validate_presence_of(:city) }
-    it { is_expected.to validate_presence_of(:zip_code) }
-    it { is_expected.to validate_presence_of(:street) }
-    it { is_expected.to validate_presence_of(:number) }
-  end
 end
