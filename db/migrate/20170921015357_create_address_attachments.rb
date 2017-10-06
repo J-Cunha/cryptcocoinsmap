648f79cb75ee5906c36cb74a625970bc914b8553
João Cunha <31866254+J-Cunha@users.noreply.github.com>
2017-10-06 20:47:22 -0300

    Update 20170921015357_create_address_attachments.rb
    Update to text type, to work with mysql

--- db/migrate/20170921015357_create_address_attachments.rb
+++ db/migrate/20170921015357_create_address_attachments.rb
@@ -2,7 +2,7 @@ class CreateAddressAttachments < ActiveRecord::Migration[5.1]
   def change
     create_table :address_attachments do |t|
       t.belongs_to :address
-      t.json :photos
+      t.text :photos
 
       t.timestamps
     end
