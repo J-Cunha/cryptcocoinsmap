2837b03b7b9daaa68f9a3f1747114a8dc0fb4f18
J-Cunha <soares.cunha.joao@gmail.com>
2017-10-06 15:16:26 -0300

    updated layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!, only: [:index, :addresses, :avaliations, :contacts]
   def index
-    @addresses = current_user.addresses
+    redirect_to edit_user_registration_path
   end
 
   def addresses
