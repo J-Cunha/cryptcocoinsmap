3befed4efb609eb9c3924bd8bfa40ad419239bb1
J-Cunha <soares.cunha.joao@gmail.com>
2017-10-10 10:56:04 -0300

    Check address owner filter

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,6 +3,15 @@ class AddressesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :update, :destroy, :edit]
   # GET /addresses
   # GET /addresses.json
+  before_action :require_permission, only: [:edit, :destroy, :update]
+
+  def require_permission
+    if current_user != Address.find(params[:id]).user
+      redirect_to root_path
+      #Or do something else here
+    end
+  end
+
   def index
     if user_signed_in?
       @addresses = current_user.addresses
@@ -10,11 +19,13 @@ class AddressesController < ApplicationController
       @addresses = Address.all
     end
   end
+
   # GET /addresses/1
   # GET /addresses/1.json
   def show
     @address_attachments = @address.address_attachments.all
   end
+
   # GET /addresses/new
   def new
     @address = Address.new
@@ -35,64 +46,66 @@ class AddressesController < ApplicationController
       if @address.save
         save_attachments if params[:address_attachments]
         @address.set_crypto_currencies_accepted = address_params[:crypto_currencies_accepted]
-        format.html { redirect_to @address, notice: 'Address was successfully created.' }
-        format.json { render :show, status: :created, location: @address }
+        format.html {redirect_to @address, notice: 'Address was successfully created.'}
+        format.json {render :show, status: :created, location: @address}
       else
-        format.html { render :new }
-        format.json { render json: @address.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @address.errors, status: :unprocessable_entity}
       end
     end
   end
+
   # PATCH/PUT /addresses/1
   # PATCH/PUT /addresses/1.json
   def update
     respond_to do |format|
       update_attchments if params[:address_attachments]
       if @address.update_attributes(address_params)
-        format.html { redirect_to @address, notice: 'Address was successfully updated.' }
-        format.json { render :show, status: :ok, location: @address }
+        format.html {redirect_to @address, notice: 'Address was successfully updated.'}
+        format.json {render :show, status: :ok, location: @address}
       else
-        format.html { render :edit }
-        format.json { render json: @address.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @address.errors, status: :unprocessable_entity}
       end
     end
   end
+
   # DELETE /addresses/1
   # DELETE /addresses/1.json
   def destroy
     @address.destroy
     respond_to do |format|
-      format.html { redirect_to addresses_url, notice: 'Address was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to addresses_url, notice: 'Address was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    
-    # Use callbacks to share common setup or constraints between actions.
-    def set_address
-      @address = Address.find(params[:id])
-    end
-    
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def address_params
-     params.require(:address).permit(:description,:business_name,:web_site,:facebook_page,:country_id, :state, :city,
-                                      :zip_code,:district,
-                                      :street, :number, :complement,
-                                      :reference_point,
-                                      :email, :phone, address_attachments_attributes: [:id, :address_id, {photos: []}, :_destroy],  category_attributes: [:id, :_destroy, :name], crypto_currencies_accepted: [] )
 
-    end
-   
-    def save_attachments
-      @address.address_attachments.each do |a|
-        @address_attachment = @address.address_attachments.create!(:photo => a)
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_address
+    @address = Address.find(params[:id])
+  end
 
-    def update_attchments
-      @address.address_attachments.each(&:destroy) if @address.address_attachments.present?
-        save_attachments       
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def address_params
+    params.require(:address).permit(:description, :business_name, :web_site, :facebook_page, :country_id, :state, :city,
+                                    :zip_code, :district,
+                                    :street, :number, :complement,
+                                    :reference_point,
+                                    :email, :phone, address_attachments_attributes: [:id, :address_id, {photos: []}, :_destroy], category_attributes: [:id, :_destroy, :name], crypto_currencies_accepted: [])
+
+  end
+
+  def save_attachments
+    @address.address_attachments.each do |a|
+      @address_attachment = @address.address_attachments.create!(:photo => a)
     end
+  end
+
+  def update_attchments
+    @address.address_attachments.each(&:destroy) if @address.address_attachments.present?
+    save_attachments
+  end
 
 end
